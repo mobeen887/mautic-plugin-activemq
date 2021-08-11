9ec9854829b2348c0144b777e05b25e27e6e14a2
Mobeen Shah <shahmobeen333@gmail.com>
2021-08-11 15:44:30 +0500

    Stomp and Sender Code added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '443d3d621534dff605065dcf7422b3e81700e504',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '443d3d621534dff605065dcf7422b3e81700e504',
+            'dev_requirement' => false,
+        ),
+        'stomp-php/stomp-php' => array(
+            'pretty_version' => '5.0.0',
+            'version' => '5.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stomp-php/stomp-php',
+            'aliases' => array(),
+            'reference' => '50f6e6e9aa1ba4696faa40cd1cbe180a96679f98',
+            'dev_requirement' => false,
+        ),
+    ),
+);
